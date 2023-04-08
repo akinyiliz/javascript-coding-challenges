0a95a4195d564e8ca8c13f4ecd190f5062fe42dc
akinyiliz <lizzieke84@gmail.com>
2023-04-08 17:27:28 +0300

    Eleventh challlenge

---
+++ 11-convert-html-entities.js
@@ -0,0 +1,29 @@
+function convertHTML(str) {
+	let charArr = str.split("");
+	for (let i = 0; i < charArr.length; i++) {
+		switch (charArr[i]) {
+			case "&":
+				charArr[i] = "&amp;";
+				break;
+			case "<":
+				charArr[i] = "&lt;";
+				break;
+			case ">":
+				charArr[i] = "&gt;";
+				break;
+			case "<>":
+				charArr[i] = "&lt;&gt;";
+				break;
+			case '"':
+				charArr[i] = "&quot;";
+				break;
+			case "'":
+				charArr[i] = "&apos;";
+				break;
+		}
+	}
+	let finalStr = charArr.join("");
+	return finalStr;
+}
+
+convertHTML("Dolce & Gabbana");
